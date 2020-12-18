f3e9631bc7551c174f473bf85ebfd3a812454de4
SingleUSTC <90564366+SingleUSTC@users.noreply.github.com>
2020-12-18 11:31:29 +0000

    make rubocop happy

---
+++ src/CryptoStore/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
@@ -0,0 +1,22 @@
+﻿namespace CryptoStore.Areas.Identity.Pages.Account.Manage
+{
+    using System.Threading.Tasks;
+    using CryptoStore.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging; 
+
+    public class ResetAuthenticatorModel : PageModel
+    {
+        UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+        ILogger<ResetAuthenticatorModel> logger;
+
+        public ResetAuthenticatorModel(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            ILogger<ResetAuthenticatorModel> logger)
+        {
+            this.userManager = userManager;
+            this
