5569f34ce1f2556f6e69486f4d1df3364a44b3a0
SingleUSTC <90564366+SingleUSTC@users.noreply.github.com>
2020-12-18 23:08:41 +0000

    Add SSL support

--- src/CryptoStore/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
+++ src/CryptoStore/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
@@ -19,4 +19,40 @@
             ILogger<ResetAuthenticatorModel> logger)
         {
             this.userManager = userManager;
-            this
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+            }
+
+            await userManager.SetTwoFactorEnabledAsync(user, false);
+            await userManager.ResetAuthenticatorKeyAsync(user);
+            logger.LogInformation("User with ID '{UserId}' has reset their authentication app key.", user.Id);
+            
+            await signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Your authenticator app key has been reset, you will need to configure your authenticator app using the new key.";
+
+            return RedirectToPage("./EnableAuthenticator");
+        }
+    }
+}
---
+++ src/CryptoStore/Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
@@ -0,0 +1,1 @@
+﻿namespace CryptoStore.Areas.Identity.Pages.
