82b6089ed6e30dc5abbb7b789cb980adc7b7f3ee
SingleUSTC <90564366+SingleUSTC@users.noreply.github.com>
2020-12-21 07:00:01 +0000

    Update version info.

--- src/CryptoStore/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
+++ src/CryptoStore/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
@@ -20,4 +20,55 @@
         private readonly UserManager<User> userManager;
         private readonly IEmailSender emailSender; 
 
-        public ResendEmailConfirmationModel(UserManager<User> userManager, IEmailSe
+        public ResendEmailConfirmationModel(UserManager<User> userManager, IEmailSender emailSender)
+        {
+            this.userManager = userManager;
+            this.emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await userManager.FindByEmailAsync(Input.Email);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email.");
+                return Page();
+            }
+
+            var userId = await userManager.GetUserIdAsync(user);
+            var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { userId = userId, code = code },
+                protocol: Request.Scheme);
+            await emailSender.SendEmailAsync(
+                Input.Email,
+                "Confirm your email",
+                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+            ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email.");
+            return Page();
+        }
+    }
+}
---
+++ src/CryptoStore/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -0,0 +1,39 @@
+﻿namespace CryptoStore.Areas.Identity.Pages.Account
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using CryptoStore.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.WebUtilities;
+
+
+    [AllowAnonymous]
+    public class ResetPasswordModel : PageModel
+    {
+        private readonly UserManager<User> userManager;
+
+        public ResetPasswordModel(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confi
