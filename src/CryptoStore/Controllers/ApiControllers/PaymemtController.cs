a3d89009414f3a96f36ffa26ed624b380b73314a
SingleUSTC <90564366+SingleUSTC@users.noreply.github.com>
2020-12-22 18:08:18 +0000

    top

---
+++ src/CryptoStore/Controllers/ApiControllers/PaymemtController.cs
@@ -0,0 +1,22 @@
+﻿namespace CryptoStore.Controllers.ApiControllers
+{
+    using CryptoStore.Infrastructure.Services;
+    using CryptoStore.Services.Contracts;
+    using CryptoStore.ViewModels.ApiModels.Payment;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    public class PaymemtController : ApiController
+    {
+        private readonly IPaymentService payment;
+        private readonly ICurrentUserService currentUser;
+        private readonly IServicesService serviceDetails;
+
+        public PaymemtController(
+            IPaymentService payment,
+            ICurrentUserService currentUser,
+            IServicesService serviceDetails) 
+        {
+            this.payment = payment;
+            this.currentUser = currentUser;
+       
